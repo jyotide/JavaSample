134ab94185f06221d5ff246f3d19dc759b488835
Saswata <saswata.jana@accenture.com>
2018-01-30 16:38:50 +0530

    just now modified

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -3,7 +3,7 @@ package org.ravi.helloworld;
 public class Hello {
 	
 	public String displayHelloMessage(String message){
-		message = "hi, this is changed";
+		message = "hi, this is just now changed";
 		return message;
 	}
 
