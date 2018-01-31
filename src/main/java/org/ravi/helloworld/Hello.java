1f4e6798687f3b3b5197dea0d55e1b930a693516
Saswata <saswata.jana@accenture.com>
2018-01-31 10:14:17 +0530

    changes

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -2,9 +2,13 @@ package org.ravi.helloworld;
 
 public class Hello {
 	
-	public String displayHelloMessage(String message){
-		message = "hi, this is just now changed";
-		return message;
+		public String displayHelloMessage(String message){
+string msg;
+if(message != null && !message.isEmpty()) {
+msg = message;}
+		
 	}
+else {msg = "hi";}
+return msg;
 
 }
