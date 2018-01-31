a98b32d1cf919b2659a032422395142698121a94
jyotide <35954964+jyotide@users.noreply.github.com>
2018-01-31 12:14:10 +0530

    Update Hello.java
    test

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -3,12 +3,8 @@ package org.ravi.helloworld;
 public class Hello {
 	
 		public String displayHelloMessage(String message){
-string msg;
-if(message != null && !message.isEmpty()) {
-msg = message;}
-		
-	}
-else {msg = "hi";}
-return msg;
+
+	       return message;
+		}
 
 }
